cab59963505760c37f4e798f78ec59bf89e63915
Александр Рахмаев <rahmaevao@gmail.com>
2022-01-07 19:00:58 +0300

    Начальный коммит

---
+++ pelicanconf.py
@@ -0,0 +1,21 @@
+AUTHOR = 'Александр Рахмаев'
+SITENAME = 'д. Янашбеляк'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Moscow'
+
+DEFAULT_LANG = 'ru'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
